0bdb24b22f802f85e9e4ba57ef89eca99c380f6a
Leonardo <leonardo.manzano@outlook.es>
2021-01-15 00:45:37 -0600

    Segundo commit

--- app/src/main/java/com/leonardo/practicando1/MainActivity.java
+++ app/src/main/java/com/leonardo/practicando1/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //otro codigo que escribo
         // proyecto para solo probar subir a github
     }
 }
